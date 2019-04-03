8be78be5dd637c14a91dc967906bab6f8816a943
FajarM <muhammadfajarm@gmail.com>
2019-04-04 01:07:28 +0700

    version 0.2

---
+++ app/src/main/java/com/morningstar/pretest_akb11_10116490/LoginCode.java
@@ -0,0 +1,41 @@
+package com.morningstar.pretest_akb11_10116490;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginCode extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_Masuk, btn_BeliKode;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_code);
+
+        btn_Masuk = findViewById(R.id.btnMasuk);
+        btn_Masuk.setOnClickListener(this);
+
+        btn_BeliKode = findViewById(R.id.btnBeliKode);
+        btn_BeliKode.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnMasuk:
+                Intent moveIntent = new Intent(LoginCode.this, Biodata.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btnBeliKode:
+                Intent moveIntent2 = new Intent(LoginCode.this, Biodata.class);
+                startActivity(moveIntent2);
+                break;
+        }
+    }
+}
--- app/src/main/java/com/morningstar/pretest_akb11_10116490/MainActivity.java
+++ app/src/main/java/com/morningstar/pretest_akb11_10116490/MainActivity.java
@@ -1,13 +1,29 @@
 package com.morningstar.pretest_akb11_10116490;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_Mulai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        btn_Mulai = (Button) findViewById(R.id.btnMulai);
+        btn_Mulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (MainActivity.this, LoginCode.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
